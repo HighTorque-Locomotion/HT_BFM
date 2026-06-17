9b51d63b9bfe55c7cd16d14e71aa91bcf29b6ef6
laihuiying <82623700@qq.com>
2026-06-17 22:17:51 +0800

    back to h20 setting

--- humanoidverse/train.py
+++ humanoidverse/train.py
@@ -734,10 +734,10 @@ def train_bfm_zero(robot: str = ROBOT_G1):
         ),
         work_dir=f"results/{robot_settings['work_dir_prefix']}-{time.strftime('%Y%m%d_%H%M%S')}",
         seed=4728,
-        online_parallel_envs=512,
-        log_every_updates=10240,
+        online_parallel_envs=1024,
+        log_every_updates=102400,
         num_env_steps=384000000,
-        update_agent_every=512,
+        update_agent_every=1024,
         num_seed_steps=10240,
         num_agent_updates=16,
         checkpoint_every_steps=9600000,
@@ -748,7 +748,7 @@ def train_bfm_zero(robot: str = ROBOT_G1):
         prioritization_scale=2.0,
         prioritization_mode='exp',
         use_trajectory_buffer=True,
-        buffer_size=2560000,
+        buffer_size=5120000,
         use_wandb=True,
         wandb_ename='82623700-dzkd',  # your wandb entity (username/team), empty = default from wandb login
         wandb_gname=robot_settings["wandb_group"],  # run group
@@ -756,7 +756,7 @@ def train_bfm_zero(robot: str = ROBOT_G1):
         load_isaac_expert_data=True,
         buffer_device='cuda',
         disable_tqdm=False,
-        evaluations=[HumanoidVerseIsaacTrackingEvaluationConfig(name='HumanoidVerseIsaacTrackingEvaluationConfig', generate_videos=False, videos_dir='videos', video_name_prefix='unknown_agent', name_in_logs='humanoidverse_tracking_eval', env=None, num_envs=512, n_episodes_per_motion=1)],
+        evaluations=[HumanoidVerseIsaacTrackingEvaluationConfig(name='HumanoidVerseIsaacTrackingEvaluationConfig', generate_videos=False, videos_dir='videos', video_name_prefix='unknown_agent', name_in_logs='humanoidverse_tracking_eval', env=None, num_envs=1024, n_episodes_per_motion=1)],
         eval_every_steps=9600000,
         tags={},
     )
