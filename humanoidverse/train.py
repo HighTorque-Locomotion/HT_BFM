456e2e7be5d19f8d46daaa1a1916a80b23ef7941
laihuiying <82623700@qq.com>
2026-06-19 06:58:02 +0800

    fix bug of piplus action range

--- humanoidverse/train.py
+++ humanoidverse/train.py
@@ -613,9 +613,6 @@ def _get_robot_training_settings(robot: str) -> dict[str, tp.Any]:
             "lafan_tail_path": "humanoidverse/data/pi_LSE_lafan_dataset_20260617_v3/piplus_lse_lafan_10s-clipped.pkl",
             "hydra_overrides": [
                 "robot=piplus/PiPlus_S_12L8A0G2H1W_LSE",
-                "robot.control.action_scale=0.25",
-                "robot.control.action_clip_value=5.0",
-                "robot.control.normalize_action_to=5.0",
                 "env.config.lie_down_init=True",
                 "env.config.lie_down_init_prob=0.3",
             ],
